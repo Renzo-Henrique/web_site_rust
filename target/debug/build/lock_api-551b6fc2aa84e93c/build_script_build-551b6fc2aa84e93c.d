16b175cf50b27ac6b41536bf0510328ce0be3987
saick123 <matheussaick@gmail.com>
2023-07-17 00:25:45 -0300

    excluindo tudo

--- target/debug/build/lock_api-551b6fc2aa84e93c/build_script_build-551b6fc2aa84e93c.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/VSCODE/PARADIGMAS/missao/target/debug/build/lock_api-551b6fc2aa84e93c/build_script_build-551b6fc2aa84e93c: /home/rhgleao/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.10/build.rs
-
-/mnt/d/VSCODE/PARADIGMAS/missao/target/debug/build/lock_api-551b6fc2aa84e93c/build_script_build-551b6fc2aa84e93c.d: /home/rhgleao/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.10/build.rs
-
-/home/rhgleao/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.10/build.rs:
