d5e50eabeb3826ae5c8ab630c718a20a0aa6747b
Renzo-Henrique <renzolealguzzo@gmail.com>
2023-07-18 21:35:42 -0300

    rust book

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,88 +1,72 @@
-#[macro_use] extern crate rocket;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
-use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
-use serde::{Serialize, Deserialize};
+use std::fs;
 
-use rocket_contrib::json::Json;
-use std::sync::Mutex;
-use rocket::State;
-
-use rocket::fs::NamedFile;
-
-
-#[derive(Deserialize, Serialize, Debug)]
-struct User{
-    name: String,
-    password: String,
-    favorite_fruit: String,
-    favorite_language: String,
-    profile_picture: String,
-}
-
-struct AppState{
-    users: Mutex<HashMap<String, User>>
-}
-
-
-#[get("/login")]
-async fn get_login() -> Option<NamedFile>{
-
-    NamedFile::open("static/login.html").await.ok()
-}
-
-/* 
-#[post("/login", data = "<form>")]
-fn login(form: Json<User>, state: &State<AppState>) -> String{
+fn main() {
     
-    let users = &state.users.lock().unwrap();
-    let name = &form.name;
-    let senha = &form.password;
-   
+    // Criação de um listener TCP que escuta em localhost na porta 7878
+    let listener = TcpListener::bind("localhost:7878").unwrap();
+    
+    // Loop que lida com cada conexão recebida pelo listener
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-    match  users.get(name) {
-        Some(user) => {
-            if user.password == senha{
-                format!("bem vindo cliente {name}")
-            }
-        }
-        _ =>{
-            format!("falha ao logar, tente novamente")
-        }
+        // Chama a função para lidar com a conexão
+        handle_connection(stream);
+       
     }
-
 }
-*/
-/* 
-#[post("/cadastro", data = "<form>")]
-fn cadastro(form: Json<User>, state: &State<AppState>) -> String{
 
-    let users = &state.users.lock().unwrap();
 
-    match users.get(&form.name) {
-        Some(x) =>{
-            format!("usuario ja cadastrado, tente fazer login")
-        }
-        _ =>{
-            users.insert(&form.name, form);
-            format!("usuario cadastrado com sucesso")
-        }
+fn get_handler(server_path: &str) -> Option<fs::File>{
+    
+    
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // Buffer para armazenar os dados recebidos pela conexão TCP
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+
+    // Verifica o tipo de requisição recebida
+    //let get = b"GET / HTTP/1.1\r\n";
+    let get = b"GET";
+    let post = b"POST";
+    
+
+    if buffer.starts_with(get){
+        get_handler(&[4..]);
     }
-
-}
-*/
-
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!!!!!"
-}
-
-#[launch]
-fn rocket() -> _ {
-    let users = HashMap::<String ,User>::new();
-    let app_state = AppState{
-        users: Mutex::new(users)
+    // --snip--
+    let (status_line, filename) = if buffer.starts_with(get) {
+        // Se a requisição for GET "/"
+        // Retorna a página inicial e o status 200 OK
+        ("HTTP/1.1 200 OK\r\n\r\n", "./public/tela_inicial.html")
+    }
+    else if buffer.starts_with(post){
+        // Se a requisição for POST
+        // Retorna a página de login ??? e o status 200 OK
+        ("HTTP/1.1 200 OK\r\n\r\n", "./public/tela_login.html")
+    }
+    else {
+        // Para qualquer outra requisição
+        // Retorna a página de erro 404 e o status 404 NOT FOUND
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "./public/404.html")
     };
 
-    rocket::build().manage(app_state).mount("/", routes![hello, get_login])
+    // Lê o conteúdo do arquivo com base no nome do arquivo recebido
+    let contents = fs::read_to_string(filename).unwrap();
+
+    // Cria a resposta HTTP a ser enviada para o cliente
+    let response = format!("{}{}", status_line, contents);
+
+    // Envia a resposta para o cliente através do stream TCP
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
